032325372b75880b0fd14dcffdfbb352db09b045
easyhooon <jihun.lee@medisolution.co.kr>
2025-03-06 09:31:58 +0900

    [chore] 프로젝트 수준 build.gradle 필요없는 plugin 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,6 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.compose.multiplatform) apply false
-    alias(libs.plugins.detekt.gradle.plugin)
-    alias(libs.plugins.kotlin.ktlint)
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.application) apply false
